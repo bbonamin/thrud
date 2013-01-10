d7140d87aaab05658d69d3a632a3a6202fff9759
Bruno Bonamin <bruno@bonamin.org>
2013-01-09 23:35:28 -0300

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Thrud::Application.config.secret_token = '29a0421c0841e8ea198659d180fa72c64a7b1697960df068953e27048d2326af9aaf42004ed9509c3a2f2d023781bd128ce87ebec3e09eef608f589b705ed145'
