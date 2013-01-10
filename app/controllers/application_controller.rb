8bc18767cbc5e78f3fa64a231e02a65005a4a3e7
Bruno Bonamin <bruno@bonamin.org>
2013-01-10 00:08:44 -0300

    change env variables implementation for heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  http_basic_authenticate_with :name => Figaro.env.http_basic_user, :password => Figaro.env.http_basic_password
+  http_basic_authenticate_with :name => ENV['HTTP_BASIC_USER'], :password => ENV['HTTP_BASIC_PASSWORD']
 end
